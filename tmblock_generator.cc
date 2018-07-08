6452a06629129732a3447a92419603bb07970eb6
Hui Yiqun <huiyiqun@gmail.com>
2018-07-08 17:59:04 +0800

    cast variables to float before computation

--- tmblock_generator.cc
+++ tmblock_generator.cc
@@ -10,15 +10,18 @@ class TMBlockGenerator : public Halide::Generator<TMBlockGenerator> {
 
     Output<Buffer<uint8_t>> output{"output", 3};
 
-    Func logo_ext{"logo_ext"};
+    Func logo_ext{"logo_ext"}, input_f{"input_f"}, logo_f{"logo_f"};
 
     Var x, y, ch;
 
     void generate() {
+        input_f(x, y, ch) = cast<float>(input(x, y, ch));
         logo_ext = BoundaryConditions::constant_exterior(logo, 0);
-        Expr alpha = logo_ext(x, y, 4);
-        output(x, y, ch) = input(x, y, ch) / (1 - alpha) -
-                           logo_ext(x, y, ch) * alpha / (1 - alpha);
+        logo_f(x, y, ch) = cast<float>(logo_ext(x, y, ch));
+        Expr alpha = logo_f(x, y, 4) / 255;
+        output(x, y, ch) =
+            cast<uint8_t>(input_f(x, y, ch) / (1 - alpha) -
+                          logo_f(x, y, ch) * alpha / (1 - alpha));
     }
 
     void schedule() { output.compute_root(); }
